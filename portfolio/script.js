6cdb745ebe2dd006a5a9867a0a056b77ad0de960
Ashray Saini <sainiashray7@gmail.com>
2024-07-19 22:14:52 +0530

    first commit

--- portfolio/script.js
+++ portfolio/script.js
@@ -49,7 +49,7 @@ ScrollReveal().reveal('.home-content p,.about-content', { origin: 'right' });
 
 /=========== typed js =======/
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'Problem solver', 'Sportsperson'],
+    strings: ['Web Development Enthusiast ', 'Problem Solver', 'SportsPerson'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
